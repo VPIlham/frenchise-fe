9bdfd7279be2d2f283fed81b7311c4fb6a02c0e1
ilham <ilhamnurhakim378@gmail.com>
2022-08-24 16:50:41 +0700

    seller : brands update

--- src/services/brand.js
+++ src/services/brand.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import { URL_API } from '../core/constant';
 
-const getBrand = async (cb) => {
+const getBrandAll = async (cb) => {
     try {
-        let brands = await axios({
+        const brands = await axios({
             method: 'GET',
             url: URL_API
         });
@@ -14,4 +14,49 @@ const getBrand = async (cb) => {
     }
 };
 
-export { getBrand };
+const getBrandBySeller = async (cb, id) => {
+    try {
+        const brands = await axios({
+            method: 'GET',
+            url: `${URL_API}/brands/${id}?populate=Upload`
+        });
+        return cb(brands.data);
+    } catch (err) {
+        return err;
+    }
+};
+
+const updateBrandBySeller = async (values) => {
+    try {
+        const formData = new FormData();
+
+        const { name, images, totalEmployees, startOperation, category, id } = values;
+
+        const tempObj = {
+            name,
+            totalEmployees,
+            startOperation,
+            category
+        };
+
+        formData.append('image', values.image, values.image.name);
+        formData.append('data', JSON.stringify(tempObj));
+
+        const brands = await axios({
+            method: 'PUT',
+            url: `${URL_API}/brands/${id}`,
+            data: formData,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        Swal.fire('Berhasil', 'Update Brand Berhasil!', 'success');
+        return brands;
+    } catch (err) {
+        console.log(err);
+        Swal.fire('Gagal', 'Update Brand Gagal!', 'error');
+        return err;
+    }
+};
+
+export { getBrandBySeller, getBrandAll, updateBrandBySeller };
--- src/views/brands/mybrand.js
+++ src/views/brands/mybrand.js
@@ -1,21 +1,129 @@
-import { FormControl, Grid, Input } from '@mui/material';
+import { FormControl, Button, Grid, Input, InputLabel, Select, MenuItem, TextField } from '@mui/material';
+import { IconDeviceFloppy } from '@tabler/icons';
+import useProfile from 'hooks/useProfile';
 import { useEffect, useState } from 'react';
+import { getBrandBySeller, updateBrandBySeller } from 'services/brand';
+import MainCard from 'ui-component/cards/MainCard';
+import moment from 'moment';
 
 const MyBrandPage = () => {
     const [isLoading, setLoading] = useState(true);
+    const [brand, setBrand] = useState([]);
+
+    const [form, setForm] = useState({
+        name: '',
+        images: '',
+        totalEmployees: '',
+        startOperation: '',
+        category: ''
+    });
+
+    const submitHandler = () => {
+        updateBrandBySeller(form);
+        // navigation('/products');
+    };
+
+    const [profile] = useProfile();
+
     useEffect(() => {
         setLoading(false);
+        getBrandBySeller((result) => {
+            setBrand(result.data);
+            setForm({
+                name: result.data.name,
+                images: result.data.images,
+                totalEmployees: result.data.totalEmployees,
+                startOperation: result.data.startOperation,
+                category: result.data.category,
+                id: result.data.id
+            });
+        }, profile.id);
     }, []);
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={8}>
-                <FormControl>
-                    <InputLabel htmlFor="my-input">Name</InputLabel>
-                    <Input id="my-input" aria-describedby="my-helper-text" />
-                </FormControl>
+        <MainCard title="My Brand">
+            <Grid container spacing={2}>
+                <Grid item xs={9} justifyItems="center" alignItems="center">
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <FormControl fullWidth>
+                                <TextField
+                                    id="my-input"
+                                    aria-describedby="my-helper-text"
+                                    label="Name"
+                                    value={form.name}
+                                    onChange={(e) => setForm({ ...form, name: e.target.value })}
+                                />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl fullWidth>
+                                <TextField
+                                    id="my-input"
+                                    aria-describedby="my-helper-text"
+                                    label="Total Employee"
+                                    type="number"
+                                    value={form.totalEmployees}
+                                    onChange={(e) => setForm({ ...form, totalEmployees: e.target.value })}
+                                />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl fullWidth>
+                                <TextField
+                                    id="date"
+                                    style={{ padding: 8 }}
+                                    label="Start Operation"
+                                    type="date"
+                                    value={moment(form.startOperation).format('YYYY-MM-DD')}
+                                    name="startOperation"
+                                    sx={{ width: 220 }}
+                                    onChange={(e) => setForm({ ...form, startOperation: e.target.value })}
+                                    InputLabelProps={{
+                                        shrink: true
+                                    }}
+                                />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl fullWidth>
+                                <InputLabel htmlFor="my-input">Category</InputLabel>
+                                <Select
+                                    id="demo-simple-category"
+                                    name="category"
+                                    value={form.category}
+                                    onChange={(e) => setForm({ ...form, category: e.target.value })}
+                                    style={{ padding: 1 }}
+                                >
+                                    <MenuItem value="Industri Makanan & Minuman">Industri Makanan & Minuman</MenuItem>
+                                    <MenuItem value="Industri Ritel">Industri Ritel</MenuItem>
+                                    <MenuItem value="Industri kecantikan dan kesehatan">Industri kecantikan dan kesehatan</MenuItem>
+                                    <MenuItem value="Industri pendidikan non formal">Industri pendidikan non formal</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <span item xs={5}>
+                                Image
+                            </span>
+                            <Button variant="contained" component="label" style={{ 'margin-left': '15px' }}>
+                                Upload File
+                                <input type="file" hidden onChange={(e) => setForm({ ...form, image: e.target.files[0] })} />
+                            </Button>
+                        </Grid>
+                        <Button
+                            variant="contained"
+                            onClick={() => submitHandler()}
+                            sx={{ mt: 3, ml: 2 }}
+                            color="success"
+                            startIcon={<IconDeviceFloppy />}
+                        >
+                            Simpan
+                        </Button>
+                    </Grid>
+                </Grid>
             </Grid>
-        </Grid>
+        </MainCard>
     );
 };
 
